6001e24bed7df88e3693f6e669b9d0824a9f2842
Ewerton Becker <ewertonbecker12@gmail.com>
2022-01-28 19:08:10 +0000

    New Project Age Checker

---
+++ exercicio/validar-idade/valida.js
@@ -0,0 +1,10 @@
+function verificar (){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.getElementById('res')
+
+    if (fano.ariaValueMax.length == 0 || fano.value > ano){
+        
+    }
+}
