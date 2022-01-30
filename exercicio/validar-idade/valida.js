0334c7910241927c7070f2277f6a43edad7e3fc8
Ewerton Becker <ewertonbecker12@gmail.com>
2022-01-30 21:35:29 +0000

    ...

--- exercicio/validar-idade/valida.js
+++ exercicio/validar-idade/valida.js
@@ -1,10 +1,14 @@
 function verificar (){
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('ano')
+    var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
 
-    if (fano.ariaValueMax.length == 0 || fano.value > ano){
-        
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('Erro, Verifique os dados')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
     }
 }
